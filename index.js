4289eacd7ea60333cc16de771d9e7ab250e22040
Jeremiah Tabb <55849254+jtabb1@users.noreply.github.com>
2021-08-20 10:13:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function bstSearch(root,val) {
+  let currentNode = root;
+  
+  while (currentNode!==null) {
+    const curVal = currentNode.value;
+    
+    if (curVal===val) {
+      return currentNode
+    } else if (curVal<val) {
+      currentNode = currentNode.left
+    } else if (curVal>val) {
+      currentNode = currentNode.right
+    }
+  }
+  return null
+}
